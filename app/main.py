09632d0a4dadf6a2e650c62b01d6ce6b671d579f
Syed Ali Hussain <ali.hussain@avanza.pk>
2026-04-20 17:46:53 +0500

    fix fly.io docker deployment

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routes import customer
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Customer CRUD API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(customer.router)
